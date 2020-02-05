16ffa794467b52e65a5a9eeed371ecfcc78bea6a
Victor <nguyentanthang9a4@gmail.com>
2020-02-05 22:01:36 +0700

    Version 1.0.0.

--- app.js
+++ app.js
@@ -8,12 +8,20 @@ const mongoose = require("mongoose");
 const config = require("./config/config");
 
 var app = express();
+app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.header("Access-Control-Allow-Credentials", true); 
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
   next();
 });
-app.use(cors());
 
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
